c95eb00eaa6a9cd2308c3b01199d9b39ee8b7639
gangz1o <gangplankwong@gmail.com>
2026-02-09 23:32:28 +0800

    🌟feat: 新增多种 115 登录方式

--- sehuatang-to-115.user.js
+++ sehuatang-to-115.user.js
@@ -641,7 +641,17 @@
           border: none;
           color: #8e8e93;
           width: 26px;
+          width: 26px;
           height: 26px;
+          border-radius: 13px;
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 14px;
+          font-weight: 500;
+          transition: all 0.2s;
+          text-decoration: none;
           border-radius: 13px;
           cursor: pointer;
           display: flex;
@@ -1039,6 +1049,11 @@
           <div class="push115-header">
               <span class="push115-header-title">115离线下载助手</span>
               <div class="push115-header-btns">
+                  <a class="push115-header-btn" href="https://github.com/gangz1o/sehuatang-to-115" target="_blank" title="访问 Github 项目主页" style="text-decoration: none;">
+                      <svg height="16" width="16" viewBox="0 0 16 16" version="1.1" fill="currentColor">
+                          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                      </svg>
+                  </a>
                   <button class="push115-header-btn" id="push115-minimize" title="最小化">−</button>
               </div>
           </div>
